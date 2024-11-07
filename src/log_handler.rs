f6a30cbe49db5f0f74c53d27f1314d7d227f9180
K-n15 <theepapon7055@gmail.com>
2024-11-07 15:15:39 +0700

    re-formatted and prepare log made a log handler

---
+++ src/log_handler.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead,BufReader,Write};
+use std::path::Path;
+
+pub struct Log{
+    dictionary : HashMap<String,Vec<String>>
+}
+
+pub fn open_log()->Option<File>{
+    if Path::new(&String::from("log.txt")).exists(){
+        let new = File::open(&String::from("log.txt"));
+        match new {
+            Ok(k)=>return Some(k),
+            Err(_e)=>None
+        }
+    } else{
+        let new = File::create(&String::from("log.txt"));
+        match new {
+            Ok(k)=>return Some(k),
+            Err(_e)=>None
+        }
+    }
+}
+
+pub fn readrecord_file(x:File)->HashMap<String,Vec<String>>{
+    let temp = BufReader::new(&x);
+    let mut hashing = HashMap::<String,Vec<String>>::new();
+    for i in temp.lines(){
+        let mut word: Vec<String> = Vec::new();
+        match i {
+            Ok(k)=>{
+                word = k.split_whitespace().into_iter().map(|x|x.to_string()).collect();
+            },
+            Err(_e)=>continue
+        }
+        let name = word[0].clone();
+        word.remove(0);
+        hashing.insert(name, word);
+    }
+    return hashing;
+}
+
+pub fn record_log(x:HashMap<String,Vec<String>>){
+    let logging = File::create(&String::from("log.txt")).expect("Inoccurace occured");
+    for mut i in x{
+        let mut line: Vec<String> = Vec::new();
+        line.push(i.0);
+        if i.1.len() >= 35 {
+            line.append(&mut i.1[25..].to_vec());
+        } else {
+        line.append(&mut i.1);
+        }
+        writeln!(&logging,"{:?}",line).expect("Unexpected error occured");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 use iced::{alignment::Horizontal::Left, widget::{button, column, container, row,text}, Alignment::Center, Element, Length::Fill, Subscription};
 use systemstat::ByteSize;
 
 mod libraries;
+mod log_handler;
 
 fn main() -> iced::Result {
     iced::application("Status Manager", Status::update, Status::view)
@@ -43,7 +46,9 @@ struct Status {
     uptime : String,
     boottime : String,
     last_update : f32,
+    log : HashMap<String,Vec<String>>
 }
+
 
 fn flush(value: &mut Status){
     value.cpu_usage = libraries::cpuusage::cpu_usage();
@@ -61,71 +66,71 @@ fn flush(value: &mut Status){
 }
 
 impl Status{
-fn update(&mut self, message: Computer) {
-    self.last_update = iced::time::Instant::now().elapsed().as_secs_f32();
-    match message {
-        Computer::CpuUsage => self.cpu_usage = libraries::cpuusage::cpu_usage(),
-        Computer::MemoryUsed => {
-            Status::update(self, Computer::MemoryTotal);
-            self.memory_used = libraries::memory::memory_usage().0
-        },
-        Computer::MemoryTotal => self.memory_total = libraries::memory::memory_usage().1,
-        Computer::DiskRead => {
-            Status::update(self, Computer::TotalRead);
-            self.disk_read = libraries::disk::disk_read().0
-        },
-        Computer::DiskWrite => {
-            Status::update(self, Computer::TotalWrite);
-            self.disk_write = libraries::disk::disk_write().0
-        },
-        Computer::TotalRead => self.tdisk_read = libraries::disk::disk_read().1,
-        Computer::TotalWrite => self.tdisk_write = libraries::disk::disk_write().1,
-        Computer::NetReceive => self.network_receive = libraries::network::network_status().0,
-        Computer::NetTransmit => self.network_transmit = libraries::network::network_status().1,
-        Computer::Battery => self.battery = libraries::battery::get_battery().remaining_capacity,
-        Computer::UpTime => self.uptime = libraries::uptime::get_uptime(),
-        Computer::BootTime => self.boottime = libraries::boottime::boot_time(),
-        Computer::RefreshAll =>{flush(self);},
+    fn update(&mut self, message: Computer) {
+        self.last_update = iced::time::Instant::now().elapsed().as_secs_f32();
+        match message {
+            Computer::CpuUsage => self.cpu_usage = libraries::cpuusage::cpu_usage(),
+            Computer::MemoryUsed => {
+                Status::update(self, Computer::MemoryTotal);
+                self.memory_used = libraries::memory::memory_usage().0
+            },
+            Computer::MemoryTotal => self.memory_total = libraries::memory::memory_usage().1,
+            Computer::DiskRead => {
+                Status::update(self, Computer::TotalRead);
+                self.disk_read = libraries::disk::disk_read().0
+            },
+            Computer::DiskWrite => {
+                Status::update(self, Computer::TotalWrite);
+                self.disk_write = libraries::disk::disk_write().0
+            },
+            Computer::TotalRead => self.tdisk_read = libraries::disk::disk_read().1,
+            Computer::TotalWrite => self.tdisk_write = libraries::disk::disk_write().1,
+            Computer::NetReceive => self.network_receive = libraries::network::network_status().0,
+            Computer::NetTransmit => self.network_transmit = libraries::network::network_status().1,
+            Computer::Battery => self.battery = libraries::battery::get_battery().remaining_capacity,
+            Computer::UpTime => self.uptime = libraries::uptime::get_uptime(),
+            Computer::BootTime => self.boottime = libraries::boottime::boot_time(),
+            Computer::RefreshAll =>{flush(self);},
+        }
     }
-}
 
-fn subscription(&self) -> Subscription<Computer> {
-    iced::time::every(iced::time::Duration::from_secs(10)).map(|_| Computer::RefreshAll)
-}
+    fn subscription(&self) -> Subscription<Computer> {
+        iced::time::every(iced::time::Duration::from_secs(10)).map(|_| Computer::RefreshAll)
+    }
 
-fn view(&self) -> Element<Computer> {
-    container(
-    column![
-        row![text(" ")],
-        row![button("Refresh").on_press(Computer::CpuUsage),
-            text(format!("CPU status : {}%",self.cpu_usage))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::MemoryUsed),
-            text(format!("Memory : {}/{} Byte",self.memory_used,self.memory_total))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::DiskRead),
-            text(format!("Disk Input : {}/{}",self.disk_read,self.tdisk_read))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::DiskWrite),
-            text(format!("Disk Output : {}/{}",self.disk_write,self.tdisk_write))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::NetReceive),
-            text(format!("Network Received : {} B",self.network_receive))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::NetTransmit),
-            text(format!("Network Transmitted : {} B",self.network_transmit))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::Battery),
-            text(format!("Battery Status : {}%",self.battery*100.0)),
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::BootTime),
-            text(format!("System booted since : {} ",self.boottime))
-        ].spacing(20),
-        row![button("Refresh").on_press(Computer::UpTime),
-            text(format!("System running since : {} ",self.uptime))
-        ].spacing(20),
-        row![button("Refresh All").on_press(Computer::RefreshAll)].spacing(20),
-    ].align_x(Left).width(Fill).height(Fill).spacing(10)
-    ).align_x(Center).align_y(Center).into()
-}
+    fn view(&self) -> Element<Computer> {
+        container(
+        column![
+            row![text(" ")],
+            row![button("Refresh").on_press(Computer::CpuUsage),
+                text(format!("CPU status : {}%",self.cpu_usage))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::MemoryUsed),
+                text(format!("Memory : {}/{} Byte",self.memory_used,self.memory_total))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::DiskRead),
+                text(format!("Disk Input : {}/{}",self.disk_read,self.tdisk_read))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::DiskWrite),
+                text(format!("Disk Output : {}/{}",self.disk_write,self.tdisk_write))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::NetReceive),
+                text(format!("Network Received : {} B",self.network_receive))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::NetTransmit),
+                text(format!("Network Transmitted : {} B",self.network_transmit))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::Battery),
+                text(format!("Battery Status : {}%",self.battery*100.0)),
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::BootTime),
+                text(format!("System booted since : {} ",self.boottime))
+            ].spacing(20),
+            row![button("Refresh").on_press(Computer::UpTime),
+                text(format!("System running since : {} ",self.uptime))
+            ].spacing(20),
+            row![button("Refresh All").on_press(Computer::RefreshAll)].spacing(20),
+        ].align_x(Left).width(Fill).height(Fill).spacing(10)
+        ).align_x(Center).align_y(Center).into()
+    }
 }
